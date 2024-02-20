06a4795507978b96745f868f818d98afdff9b789
goyadev <goyazheng@gmail.com>
2024-02-20 15:54:04 +0800

    added icon for search and dark mode toggle, need to reposition with flex to the right hand side

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,22 +4,31 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <>
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/projects">PROJECTS</Link>
-          </li>
-          <li>
-            <Link to="/contact">CONTACT</Link>
-          </li>
-          <li>
-            <Link to="/login">LOGIN</Link>
-          </li>
-        </ul>
-      </nav>
+      <section className="navsection">
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/projects">PROJECTS</Link>
+            </li>
+            <li>
+              <Link to="/contact">CONTACT</Link>
+            </li>
+            <li>
+              <Link to="/login">LOGIN</Link>
+            </li>
+          </ul>
+        </nav>
+        <img src="/search.png" className="icon" id="search" alt="searchbar" />
+        <img
+          src="/switch-dark.png"
+          className="icon"
+          id="switchdark"
+          alt="dark mode toggle"
+        />
+      </section>
       <Outlet />
     </>
   );
