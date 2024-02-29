f973088c0d2bdeb82eb80137b5f79b16ec9ce4c2
goyadev <goyazheng@gmail.com>
2024-02-29 21:08:12 +0800

    fixed date from isostring to locale date in project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,12 +7,18 @@ import CreatePledgeForm from "./CreatePledge";
 function ProjectCard(props) {
   const { projectData } = props;
   // const projectLink = `project/${projectData.id}`;
+  var date = new Date(projectData.date_created);
+  var formattedDate = date.toLocaleDateString();
 
   return (
     <div className="project-card">
       <Link to={`/project/${projectData.id}`}>
         <img src={projectData.image} />
         <h3>{projectData.title}</h3>
+        <h4>Started: {formattedDate}</h4>
+        <h4>Created by: {projectData.owner}</h4>
+        <h4>{projectData.description}</h4>
+        <h4>Project Goal: {projectData.goal}</h4>
       </Link>
       <CreatePledgeForm projectId={projectData.id} />
     </div>
